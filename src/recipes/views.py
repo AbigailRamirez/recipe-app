cbb1aeaa53c3a3779f89235252edd47edfce8a9a
AbigailRamirez <ramirezabigail01@gmail.com>
2023-09-19 11:58:57 -0400

    implementing login required for views

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -10,14 +12,16 @@ def welcome(request):
     return render(request, 'recipes/recipes_home.html')
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/main.html'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
 
+
+@login_required
 def records(request):
 
     return render(request, 'recipes/search.html')
