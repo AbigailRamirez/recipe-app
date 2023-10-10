4a3aa7630ff57ec38352ff4a390b45e919659b32
AbigailRamirez <ramirezabigail01@gmail.com>
2023-10-10 13:42:48 -0400

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -33,8 +33,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 if not IS_HEROKU:
     DEBUG = True
 
+#try debug - true so you can see error... model error models.py?
 if IS_HEROKU:
-    DEBUG = False
+    DEBUG = True
 
 if IS_HEROKU:
     ALLOWED_HOSTS = ['*']
