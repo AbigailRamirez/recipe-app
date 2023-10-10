e17543f40b95fcfe2b7f14e87055ac9d34ef14af
AbigailRamirez <ramirezabigail01@gmail.com>
2023-10-10 12:31:06 -0400

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['fathomless-brook-46012-221c6d6172b8.herokuapp.com/', '*']
+ALLOWED_HOSTS = ['https://fast-reaches-00663-3ebd10588880.herokuapp.com/', '*']
 
 
 # Application definition
